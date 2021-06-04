cb21d621a8f96361f6d661c217320e9fcc2373a5
1140156495 <85267948+1140156495@users.noreply.github.com>
2021-06-04 20:34:54 +0800

    Update day-2.py

--- day-2.py
+++ day-2.py
@@ -6,7 +6,7 @@ num = random.randint(bot, top)
 print('Your code in ['+ str(bot) + ',' + str(top) + '] ')
 money= 5000
 i = 0
-while i < 15:
+while i < 10:
     number = input("please input your code：")
     number = int(number)
     if i<10:
@@ -16,11 +16,11 @@ while i < 15:
         elif number < num:
             money = money - 500
             print("too small | balance：",money)
-        elif number==num:
+        else :
             money = money + 3000
             print("congratualations The correct code is ：",num,"Reward--$3000，balance：",money)
             break
-    elif i>=10:
+    else :
         print("Clocked!!!")
         break
     i=i+1
